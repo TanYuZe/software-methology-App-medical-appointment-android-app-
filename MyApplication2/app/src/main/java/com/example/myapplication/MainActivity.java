74f223e936e2b1236dc1d7d343ecfde43fb73835
TanYuZe <yuze12@hotmail.com>
2021-10-12 17:40:38 +0800

    added register UI

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    EditText User_Email = (EditText) findViewById(R.id.User_Email);
-    EditText User_Pw = (EditText) findViewById(R.id.User_Pw);
-    String user_email = User_Email.getText().toString();
-    String user_password = User_Pw.getText().toString();
 
-    public void SignInFunction(View view)
+
+
+    public void SignInFunction(View v)
     {
+        EditText User_Email = (EditText) findViewById(R.id.User_Email);
+        EditText User_Pw = (EditText) findViewById(R.id.User_Pw);
+        String user_email = User_Email.getText().toString();
+        String user_password = User_Pw.getText().toString();
+        if (user_email == "" || user_password == "")
+        {
+            Toast.makeText(this, "A username and a password are required." ,Toast.LENGTH_SHORT).show();
+        }
+        Log.i("my activity", user_email);
+        Log.i("my activity", user_password);
 
-        Log.i( "Info", "button pressed");
 
 
+    }
+
+    public void SignUpFunction(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, Register.class);
+        startActivity(intent);
 
     }
 
@@ -29,5 +44,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
