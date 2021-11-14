77096e096e4199ca55fd770bfa2742de58a50c2c
TanYuZe <yuze12@hotmail.com>
2021-11-14 19:10:28 +0800

    update UIs

--- MyApplication2/app/src/main/java/com/example/myapplication/Patient/Patient_UpdateInfo.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Patient/Patient_UpdateInfo.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.Admin;
+package com.example.myapplication.Patient;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
-public class Admin_UpdateInfo extends AppCompatActivity {
+public class Patient_UpdateInfo extends AppCompatActivity {
     EditText et_name, et_password, et_email, et_phoneno;
     Button update_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_update_info);
-        et_name = findViewById(R.id.et_name4);
-        et_password = findViewById(R.id.et_password4);
-        et_phoneno = findViewById(R.id.et_phoneno4);
-        et_email = findViewById(R.id.et_email4);
-        update_btn = findViewById(R.id.admin_update_btn);
+        setContentView(R.layout.activity_patient_update_info);
+        et_name = findViewById(R.id.et_name3);
+        et_password = findViewById(R.id.et_password3);
+        et_phoneno = findViewById(R.id.et_phoneno3);
+        et_email = findViewById(R.id.et_email3);
+        update_btn = findViewById(R.id.patient_update_btn3);
 
 
         update_btn.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +30,5 @@ public class Admin_UpdateInfo extends AppCompatActivity {
 
             }
         });
-
     }
 }
--- MyApplication2/app/src/main/java/com/example/myapplication/Pharmacist/Pharmacist_UpdateInfo.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Pharmacist/Pharmacist_UpdateInfo.java
@@ -1,7 +1,6 @@
-package com.example.myapplication.Admin;
+package com.example.myapplication.Pharmacist;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,27 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
-public class Admin_UpdateInfo extends AppCompatActivity {
+public class Pharmacist_UpdateInfo extends AppCompatActivity {
     EditText et_name, et_password, et_email, et_phoneno;
     Button update_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_update_info);
+        setContentView(R.layout.activity_pharmacist_update_info);
         et_name = findViewById(R.id.et_name4);
         et_password = findViewById(R.id.et_password4);
         et_phoneno = findViewById(R.id.et_phoneno4);
         et_email = findViewById(R.id.et_email4);
-        update_btn = findViewById(R.id.admin_update_btn);
-
-
-        update_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        update_btn = findViewById(R.id.pharmacist_update_btn);
     }
 }
